c850a31a80e5d373d5de1167d7978691ef0e77a4
Ivo Blöchliger <ivo.bloechliger@ksbg.ch>
2021-08-28 11:43:40 +0200

    exercise can be run on user input

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,31 @@
 #include <Arduino.h>
 #include "exercises.h"
 
+void run_exercise() {
+  ex1_fizz_buzz();
+}
+
 void setup() {
   Serial.begin(115200);
-  sleep(200); // Wait a little while, so all output will be shown
+  delay(200); // Wait a little while, so all output will be shown
 
   // Call the exercise
-  ex1_fizz_buzz();
+  run_exercise();
 
 }
 
+unsigned long nextMessage=0;
+
 void loop() {
-  Serial.print('.');  // Just print the single character (of type char, in contrast to ".", which is of type char*)
-  sleep(2000);
+  if (millis()>nextMessage) {
+    Serial.printf("[%lu] Type Enter to rerun the exercise\n", millis());
+    nextMessage=millis()+5000; // Every 5 seconds
+  }
+  if (Serial.available()) {  // Do we user input?
+    run_exercise();
+    delay(2);  // Return might send to characters "\r\n", so wait for the second to arrive...
+    while(Serial.available()) {  // Clear all incoming data
+      Serial.read();
+    }
+  }
 }
